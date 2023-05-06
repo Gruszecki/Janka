e56fc62933503e65aa61a23ebd085d2e27ae3c0a
Gruszecki <wojtek.gruszecki@gmail.com>
2023-05-06 17:32:27 +0200

    Add add_station method and test class

--- player.py
+++ player.py
@@ -8,49 +8,65 @@ logging.basicConfig(level = 0)
 class RadioStationIterator:
     def __init__(self):
         self.curr = {'id': 0, 'name': '', 'url': ''}
-
-        raw_urls_from_file = self.__get_urls_from_file__()
-        if raw_urls_from_file:
-            self.__urls_list__ = [{'id': r_id, 'name': r_name, 'url': r_url} for r_id, r_name, r_url in raw_urls_from_file]
-            self.__urls_list__.insert(0, self.curr)
-        else:
-            self.__urls_list__ = [self.curr]
-            logging.error('Failed to open or import raw urls from file.')
+        self.__parsed_stations_list__ = self.__get_urls_from_file__()
 
     def __get_urls_from_file__(self):
         try:
             with open(radio_settings.URLS_PATH) as urls:
-                return [list(map(str.strip, line.split(';'))) for line in urls]
+                raw_list = [list(map(str.strip, line.split(';'))) for line in urls]
+
+            if raw_list:
+                radio_stations_list = [{'id': r_id, 'name': r_name, 'url': r_url} for r_id, r_name, r_url in raw_list]
+                radio_stations_list.insert(0, {'id': 0, 'name': '', 'url': ''})
+            else:
+                radio_stations_list = [{'id': 0, 'name': '', 'url': ''}]
+                logging.info('File with urls is empty.')
+
+            return radio_stations_list
+
         except:
-            return 0
+            logging.error('Failed to open file with urls or failed to parse raw urls to radio stations.')
+            return [self.curr]
+
 
     def get_curr(self) -> dict:
         return self.curr
 
     def get_next(self) -> dict:
-        curr_index = self.__urls_list__.index(self.curr)
+        curr_index = self.__parsed_stations_list__.index(self.curr)
 
-        if curr_index < len(self.__urls_list__) - 1:
-            self.curr = self.__urls_list__[curr_index + 1]
+        if curr_index < len(self.__parsed_stations_list__) - 1:
+            self.curr = self.__parsed_stations_list__[curr_index + 1]
         else:
-            self.curr = self.__urls_list__[0]
+            self.curr = self.__parsed_stations_list__[0]
 
         return self.curr
 
     def get_prev(self) -> dict:
-        curr_index = self.__urls_list__.index(self.curr)
+        curr_index = self.__parsed_stations_list__.index(self.curr)
 
         if curr_index:
-            self.curr = self.__urls_list__[curr_index - 1]
+            self.curr = self.__parsed_stations_list__[curr_index - 1]
         else:
-            self.curr = self.__urls_list__[len(self.__urls_list__) - 1]
+            self.curr = self.__parsed_stations_list__[len(self.__parsed_stations_list__) - 1]
 
         return self.curr
+
+    def add_station(self, name: str, url: str) -> None:
+        with open(radio_settings.URLS_PATH, 'r+') as read_file, open(radio_settings.URLS_PATH, 'a') as write_file:
+            read_file_list = list(read_file)
+            if len(read_file_list):
+                last_id = int(read_file_list[-1].split(';')[0])
+                write_file.write(f'\n{last_id + 1};{name};{url}')
+            else:
+                write_file.write(f'1;{name};{url}')
+
+        self.__parsed_stations_list__ = self.__get_urls_from_file__()
 
 
 class Player:
     def __init__(self):
-        self.__urls__ = RadioStationIterator()
+        self.radio_control = RadioStationIterator()
         self.vlc_instance = vlc.Instance()
         self.media_player = self.vlc_instance.media_player_new()
 
@@ -65,13 +81,12 @@ class Player:
             logging.info(' Setting station. Initial station reached. Turning off the radio...')
 
     def set_next_station(self):
-        next_station = self.__urls__.get_next()
+        next_station = self.radio_control.get_next()
         self.__play_next_station__(next_station)
 
     def set_prev_station(self):
-        next_station = self.__urls__.get_prev()
+        next_station = self.radio_control.get_prev()
         self.__play_next_station__(next_station)
 
     def get_curr_station(self):
-        curr_station = self.__urls__.get_curr()
-        logging.info(f' Current station: id: {curr_station["id"]}, name: {curr_station["name"]}, {curr_station["url"]}')
+        return self.radio_control.get_curr()
---
+++ player_tests.py
@@ -0,0 +1,114 @@
+import unittest
+
+import radio_settings
+from player import Player
+
+
+class PlayerTests(unittest.TestCase):
+    urls_content = str()
+
+    def setUp(self) -> None:
+        with open(radio_settings.URLS_PATH, 'r') as f:
+            self.urls_content = f.read()
+
+    def tearDown(self) -> None:
+        with open(radio_settings.URLS_PATH, 'w') as f:
+            f.write(self.urls_content)
+
+    def test_001_import_radio_stations(self):
+        player = Player()
+        self.assertGreater(len(player.radio_control.__parsed_stations_list__), 1)
+
+    def test_002_initial_station_only_when_no_stations_in_file(self):
+        with open(radio_settings.URLS_PATH, 'w'):
+            pass
+
+        player = Player()
+        self.assertEqual(len(player.radio_control.__parsed_stations_list__), 1)
+
+        player.set_next_station()
+        self.assertEqual(player.get_curr_station()['id'], 0)
+
+    def test_003_approach_initial_station_after_last_station(self):
+        player = Player()
+        num_of_stations = len(player.radio_control.__parsed_stations_list__)
+
+        if num_of_stations == 1:
+            player.radio_control.add_station('Antyradio', 'https://an06.cdn.eurozet.pl/ant-web.mp3?t=1683367429522?redirected=06')
+            num_of_stations = len(player.radio_control.__parsed_stations_list__)
+
+        for _ in range(num_of_stations):
+            player.set_next_station()
+
+        self.assertEqual(player.get_curr_station()['id'], 0)
+
+    def test_004_approach_last_station_before_initial_station(self):
+        player = Player()
+        num_of_stations = len(player.radio_control.__parsed_stations_list__)
+
+        if num_of_stations == 1:
+            player.radio_control.add_station('Antyradio', 'https://an06.cdn.eurozet.pl/ant-web.mp3?t=1683367429522?redirected=06')
+
+        player.set_prev_station()
+
+        self.assertNotEqual(player.get_curr_station()['id'], 0)
+
+    def test_005_set_next_station(self):
+        player = Player()
+
+        if len(player.radio_control.__parsed_stations_list__) == 1:
+            player.radio_control.add_station('Antyradio', 'https://an06.cdn.eurozet.pl/ant-web.mp3?t=1683367429522?redirected=06')
+
+        player.set_next_station()
+
+        self.assertGreater(int(player.get_curr_station()['id']), 0)
+
+    def test_006_set_prev_station(self):
+        player = Player()
+
+        if len(player.radio_control.__parsed_stations_list__) == 1:
+            player.radio_control.add_station('Antyradio', 'https://an06.cdn.eurozet.pl/ant-web.mp3?t=1683367429522?redirected=06')
+
+        player.set_next_station()
+        self.assertGreater(int(player.get_curr_station()['id']), 0)
+        player.set_prev_station()
+        self.assertEqual(int(player.get_curr_station()['id']), 0)
+
+
+    def test_007_add_new_station_to_not_empty_file(self):
+        player = Player()
+        num_of_stations = len(player.radio_control.__parsed_stations_list__)
+        player.radio_control.add_station('Antyradio', 'https://an06.cdn.eurozet.pl/ant-web.mp3?t=1683367429522?redirected=06')
+
+        for _ in range(num_of_stations + 1):
+            player.set_next_station()
+
+        self.assertEqual(player.get_curr_station()['id'], 0)
+
+    def test_008_add_new_station_to_empty_file(self):
+        with open(radio_settings.URLS_PATH, 'w'):
+            pass
+
+        player = Player()
+        num_of_stations = len(player.radio_control.__parsed_stations_list__)
+        player.radio_control.add_station('Antyradio', 'https://an06.cdn.eurozet.pl/ant-web.mp3?t=1683367429522?redirected=06')
+
+        player.set_next_station()
+        self.assertEqual(player.get_curr_station()['id'], '1')
+
+        player.set_next_station()
+        self.assertEqual(player.get_curr_station()['id'], 0)
+
+    def test_008_initial_station_when_open_file_failed(self):
+        with open(radio_settings.URLS_PATH, 'w'):
+            pass
+
+        player = Player()
+        self.assertEqual(len(player.radio_control.__parsed_stations_list__), 1)
+
+        player.set_next_station()
+        self.assertEqual(player.get_curr_station()['id'], 0)
+
+
+if __name__ == '__main__':
+    unittest.main()
