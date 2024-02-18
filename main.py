3654e9c55fe87a0b800eb30b0774e14a5fec5c6d
Gruszecki <wojtek.gruszecki@gmail.com>
2024-02-18 16:50:01 +0100

    Update main

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
 import threading
 
+import epaper
 from alarm import Alarm
 from player import Player
 from voice_assistant import VoiceAssistant
-
-import epaper
-
 
 player = Player()
 alarm = Alarm()
@@ -14,8 +12,9 @@ voice_assistant = VoiceAssistant(player)
 voice_assistant_thread = threading.Thread(target=voice_assistant.listen_all_the_time)
 voice_assistant_thread.start()
 
+epaper_thread = threading.Thread(target=epaper.run)
+epaper_thread.start()
+
 alarm_thread = threading.Thread(target=alarm.start(player))
 alarm_thread.start()
 
-epaper_thread = threading.Thread(target=epaper.run)
-epaper_thread.start()
