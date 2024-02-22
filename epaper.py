c21e95ddcc80c9cc0cfa071b25d38471511988f9
Gruszecki <wojtek.gruszecki@gmail.com>
2024-02-22 14:39:32 +0100

    Update epaper.py

--- epaper.py
+++ epaper.py
@@ -38,7 +38,7 @@ def run(player) -> None:
 				
 				if radio_name_width > 200:
 					if radio_name_pos + int(radio_name_width * 1.5) > 0:
-						radio_name_pos - 10
+						radio_name_pos -= 10
 						radio_name_pos_bis = radio_name_pos + int(radio_name_width * 1.5)
 					else:
 						radio_name_pos = 0
