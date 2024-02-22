7119da11982da0b1f846d8260b675a5963b6d1be
Gruszecki <wojtek.gruszecki@gmail.com>
2024-02-22 10:20:59 +0100

    Add args to threads

--- main.py
+++ main.py
@@ -15,6 +15,7 @@ voice_assistant_thread.start()
 epaper_thread = threading.Thread(target=epaper.run, args=(player,))
 epaper_thread.start()
 
-alarm_thread = threading.Thread(target=alarm.start(player))
+alarm_thread = threading.Thread(target=alarm.start, args=(player,))
 alarm_thread.start()
+
 
