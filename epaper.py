bee11db4277a1acc200574575d099e4275c8a2f7
Gruszecki <wojtek.gruszecki@gmail.com>
2024-02-24 12:16:02 +0100

    Display weather

--- epaper.py
+++ epaper.py
@@ -4,6 +4,11 @@ from PIL import Image, ImageDraw, ImageFont
 
 from waveshare_epd import epd1in54_V2
 
+import weather
+
+
+def get_weather() -> str:
+	return f'{weather.get_current_weather_full_deccription()}{weather.get_daily_forecast()}'
 
 def run(player) -> None:
 	try:
@@ -15,20 +20,26 @@ def run(player) -> None:
 		image = Image.new('1', (epd.height, epd.width), 255)
 		draw = ImageDraw.Draw(image)
 		font = ImageFont.truetype('/usr/share/fonts/truetype/dejavu/DejaVuSans-Bold.ttf', 24)
+		font_weather = ImageFont.truetype('/usr/share/fonts/truetype/dejavu/DejaVuSans-Bold.ttf', 16)
+		current_weather = get_weather()
 
 		curr_station_id = 0
 		radio_name_pos = 0
 		radio_name_pos_bis = 0
+		weather_pos = 0
+		weather_pos_bis = font_weather.getsize(current_weather)[0]
 
 		while True:
 			draw.rectangle((0, 0, epd.height, epd.width), fill=255)
-
+			
+			# Draw radio station's logo
 			if player.get_curr_station().id != curr_station_id:
 				curr_station_id = player.get_curr_station().id 
 				radio_name_pos = 0
 
 			logo = Image.open(f'images/radio_{curr_station_id}.bmp')
-			
+
+			# Write radio station's name
 			if curr_station_id:
 				image.paste(logo, (50, 0))
 	
@@ -47,12 +58,27 @@ def run(player) -> None:
 					draw.text((radio_name_pos_bis, 110), player.get_curr_station().name, font=font, fill=0)
 			else:
 				image.paste(logo, (25, 0))
-	
-			current_time = time.strftime('%H:%M:%S')
-			draw.text((40, 150), current_time, font=font, fill=0)
 
+			# Write time
+			draw.text((40, 150), time.strftime('%H:%M:%S'), font=font, fill=0)
+
+			# Write weather
+			if int(time.strftime('%M')) % 15 == 0:
+				current_weather = get_weather()
+
+			draw.text((weather_pos, 180), current_weather, font=font_weather, fill=0)
+			draw.text((weather_pos_bis, 180), current_weather, font=font_weather, fill=0)
+
+			weather_pos -= 10
+			weather_pos_bis -= 10
+
+			if weather_pos_bis <= 0:
+				weather_pos = 0
+				weather_pos_bis = font_weather.getsize(current_weather)[0]
+			
+			# Display iamge
 			epd.displayPart(epd.getbuffer(image))
-			time.sleep(0.5)
+			time.sleep(0.9)
 
 	except IOError as e:
 		print(e)
